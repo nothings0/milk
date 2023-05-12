4ae4b45ead67ea6db3e84d2e6d57afb0fb07c4bb
Nothings0 <nhanphamx3@gmail.com>
2023-05-12 16:11:29 +0700

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    }
+});
